368945908cb8d67827953b26105833515c7ab4a8
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-15 21:04:10 +0330

    feat: Add models

--- concert/ticketsales/models.py
+++ concert/ticketsales/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class concertModel(models.Model):
+    Name=models.CharField(max_length=100)
+    SingerName=models.CharField(max_length=100)
+    Price=models.FloatField()
+    Time=models.IntegerField()
+    def __str__(self):
+        return self.Name
+class locationModel(models.Model):
+    Name=models.CharField(max_length=100)
+    Address=models.CharField(max_length=250)
+    Phone=models.CharField(max_length=11)
+    Capacity=models.IntegerField()
+    def __str__(self):
+        return self.Name
+        
