02cede8879a0098989c2844c99339e5160f71953
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-16 22:07:13 +0330

    Migrate new Models

--- concert/ticketsales/models.py
+++ concert/ticketsales/models.py
@@ -11,8 +11,12 @@ class concertModel(models.Model):
 class locationModel(models.Model):
     Name=models.CharField(max_length=100)
     Address=models.CharField(max_length=250)
-    Phone=models.CharField(max_length=11)
+    Phone=models.CharField(max_length=11,null=True)
     Capacity=models.IntegerField()
     def __str__(self):
         return self.Name
-        
+class timeModel(models.Model):
+    concertModel=models.ForeignKey(to=concertModel,on_delete=models.PROTECT)
+    locationModel=models.ForeignKey(to=locationModel,on_delete=models.PROTECT)
+    startDateTime=models.DateTimeField()
+    capacity=models.IntegerField()
