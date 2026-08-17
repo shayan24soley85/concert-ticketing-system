36a2080a9f79b9e9c894215303e3b93092a94937
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-17 23:06:04 +0330

    style: update models to follow PEP 8 guidelines

--- concert/ticketsales/models.py
+++ concert/ticketsales/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 # Create your models here.
-class concertModel(models.Model):
+class ConcertModel(models.Model):
     Name=models.CharField(max_length=100)
-    SingerName=models.CharField(max_length=100)
+    singer_name=models.CharField(max_length=100)
     Price=models.FloatField()
     Time=models.IntegerField()
     def __str__(self):
         return self.Name
-class locationModel(models.Model):
+class LocationModel(models.Model):
     Name=models.CharField(max_length=100)
     Address=models.CharField(max_length=250)
     Phone=models.CharField(max_length=11,null=True)
     Capacity=models.IntegerField()
     def __str__(self):
         return self.Name
-class timeModel(models.Model):
-    concertModel=models.ForeignKey(to=concertModel,on_delete=models.PROTECT)
-    locationModel=models.ForeignKey(to=locationModel,on_delete=models.PROTECT)
-    startDateTime=models.DateTimeField()
+class TimeModel(models.Model):
+    concert_model=models.ForeignKey(to=ConcertModel,on_delete=models.PROTECT)
+    location_model=models.ForeignKey(to=LocationModel,on_delete=models.PROTECT)
+    start_datetime=models.DateTimeField()
     capacity=models.IntegerField()
     started = 1
     finished = 2
@@ -35,13 +35,13 @@ class timeModel(models.Model):
     
     status = models.IntegerField(choices=status_choice)
     def __str__(self):
-        return "Time:{}\nConcertName:{}\nLocation:{}".format(self.startDateTime,self.concertModel.Name,self.locationModel.Name)
-class userModel(models.Model):
+        return "Time:{}\nConcertName:{}\nLocation:{}".format(self.start_datetime,self.concert_model.Name,self.location_model.Name)
+class UserModel(models.Model):
     Name= models.CharField(max_length=50)
     Family=models.CharField(max_length=50)
 
     
-    phoneNumber = models.CharField(
+    phone_number = models.CharField(
         max_length=11,
         validators=[MinLengthValidator(11)], 
         null=True, 
@@ -58,13 +58,13 @@ class userModel(models.Model):
     )
     def __str__(self):
         return "{} {}".format(self.Name,self.Family)
-class ticketModel(models.Model):
-    timeModel=models.ForeignKey(to=timeModel,on_delete=models.PROTECT)
-    userModel=models.ForeignKey(to=userModel,on_delete=models.PROTECT)
+class TicketModel(models.Model):
+    time_model=models.ForeignKey(to=TimeModel,on_delete=models.PROTECT)
+    user_model=models.ForeignKey(to=UserModel,on_delete=models.PROTECT)
     amount=models.IntegerField()
-    def totalPrice(self):
-        return self.timeModel.concertModel.Price*self.amount
+    def total_price(self):
+        return self.time_model.concert_model.Price*self.amount
 
     def __str__(self):
-        return "Ticket info:\n{}\nCustomer:{}\nPrice:{}".format(self.timeModel.__str__(),self.userModel.__str__(),self.totalPrice())
+        return "Ticket info:\n{}\nCustomer:{}\nPrice:{}".format(self.time_model.__str__(),self.user_model.__str__(),self.total_price())
     
