512ef447810bca79e38e1e50b39ed83f71d1dde6
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-18 22:25:41 +0330

    Register models to admin panel

--- concert/ticketsales/admin.py
+++ concert/ticketsales/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from ticketsales.models import ConcertModel
+from ticketsales.models import TicketModel
+from ticketsales.models import TimeModel
+from ticketsales.models import UserModel
+from ticketsales.models import LocationModel
 
 # Register your models here.
+admin.site.register(TicketModel)
+admin.site.register(TimeModel)
+admin.site.register(UserModel)
+admin.site.register(ConcertModel)
+admin.site.register(LocationModel)
