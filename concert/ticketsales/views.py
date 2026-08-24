dfd5126ef246aafef9c7de1235015893a80fc540
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-24 18:38:55 +0330

    Fix concert list bug

--- concert/ticketsales/views.py
+++ concert/ticketsales/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from ticketsales.models import ConcertModel
 
 
 # Create your views here.
 def concert_list_view(request):
-    return HttpResponse("لیست کنسرت های موجود")
+    concerts = ConcertModel.objects.all()
+    return HttpResponse(concerts)
