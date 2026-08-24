1343754ece9cf71af0450217b1a51bf1b6299056
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-24 17:53:00 +0330

    Add  HttpResponse & Implement  simple concert_list_view

--- concert/ticketsales/views.py
+++ concert/ticketsales/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+
 
 # Create your views here.
+def concert_list_view(request):
+    return HttpRequest("لیست کنسرت های موجود")
