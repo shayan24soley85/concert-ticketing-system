82e84a220226fc966d35690021d61cce7d625c20
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-24 20:21:00 +0330

    Add HTML tag

--- concert/ticketsales/views.py
+++ concert/ticketsales/views.py
@@ -6,4 +6,18 @@ from ticketsales.models import ConcertModel
 # Create your views here.
 def concert_list_view(request):
     concerts = ConcertModel.objects.all()
-    return HttpResponse(concerts)
+
+    text = """
+    <!DOCTYPE html>
+    <html>
+        <head></head>
+        <body>
+            <h1>لیست کنسرت ها</h1>
+            <ul>
+                {}
+            </ul>
+        </body>
+    </html>
+    """.format("\n".join("<li>{}</li>".format(concert) for concert in concerts))
+
+    return HttpResponse(text)
