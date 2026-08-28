d792df20d401ca6fafb0c0b82fc01fd197dfc8b4
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-28 17:50:43 +0330

    Update: Replace HttpResponse with render in concert view

--- concert/ticketsales/views.py
+++ concert/ticketsales/views.py
@@ -1,23 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from ticketsales.models import ConcertModel
 
 
 # Create your views here.
 def concert_list_view(request):
     concerts = ConcertModel.objects.all()
+    context = {"concert_list": concerts}
 
-    text = """
-    <!DOCTYPE html>
-    <html>
-        <head></head>
-        <body>
-            <h1>لیست کنسرت ها</h1>
-            <ul>
-                {}
-            </ul>
-        </body>
-    </html>
-    """.format("\n".join("<li>{}</li>".format(concert) for concert in concerts))
-
-    return HttpResponse(text)
+    return render(request, "ticketsales/concert_list.html", context)
