911808421b11dbe8216f0b30a2404baad33f65fa
shayan24soley <shayan.soleimani85@gmail.com>
2026-08-28 19:19:58 +0330

    Add concert_count

--- concert/ticketsales/views.py
+++ concert/ticketsales/views.py
@@ -5,6 +5,9 @@ from ticketsales.models import ConcertModel
 # Create your views here.
 def concert_list_view(request):
     concerts = ConcertModel.objects.all()
-    context = {"concert_list": concerts}
+    context = {
+        "concert_list": concerts,
+        "concert_count": concerts.count(),
+    }
 
     return render(request, "ticketsales/concert_list.html", context)
